60de2a6956b25c8f654469c2d2e0fb798e5ed049
Tushar Nikam <nikamtushar46@gmail.com>
2025-07-31 14:42:00 +0530

    no commit

--- QuickShow/Server/Server.js
+++ QuickShow/Server/Server.js
@@ -21,4 +21,6 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send('Server is Live..!'))
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.listen(port, () => console.log(`Server Listening at http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
